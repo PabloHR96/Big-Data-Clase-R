8c2036ad799f57c690a17f69b543397dd43fbc72
PabloHR96 <pherreraruiz@gmail.com>
2020-05-02 10:28:21 -0400

    Clase de R hasta ejercicio de función que arroje TRUE o FALSE si el número es par o impar

---
+++ Clase.R
@@ -0,0 +1,18 @@
+nume <- 6
+deno <- 4
+r <- nume/deno
+
+r
+
+r <- sqrt(4)
+r
+suma <- function(x,y)
+r <- x+y  
+return(r)
+
+r <- suma(2,3)
+r
+
+#Hacer una función que retorne TRUE o FALSE cuando el número es par o impar
+
+(suma <- function(x,y) && 
